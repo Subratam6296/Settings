NAMELESS-AOSP-OPENSOURCE/packages_apps_Settings

Settings: Refactor qs tiles layout fragment
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

src/com/android/settings/display/QsTileLayoutSettings.java
@@ -14,51 +14,104 @@
 
 package com.android.settings.display;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.custom.CustomUtils.QSLayoutUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.android.settings.custom.preference.SystemSettingSeekBarPreference;
+import com.android.settingslib.widget.LayoutPreference;
+
+import com.android.settings.custom.preference.CustomSeekBarPreference;
 import com.android.settings.custom.preference.SystemSettingSwitchPreference;
 
 public class QsTileLayoutSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_QS_ROW_PORTRAIT = "qs_layout_rows";
-    private static final String KEY_QQS_ROW_PORTRAIT = "qqs_layout_rows";
     private static final String KEY_QS_HIDE_LABEL = "qs_tile_label_hide";
     private static final String KEY_QS_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
+    private static final String KEY_QS_COLUMN_PORTRAIT = "qs_layout_columns";
+    private static final String KEY_QS_ROW_PORTRAIT = "qs_layout_rows";
+    private static final String KEY_QQS_ROW_PORTRAIT = "qqs_layout_rows";
+    private static final String KEY_APPLY_CHANGE_BUTTON = "apply_change_button";
 
-    private SystemSettingSeekBarPreference mQsRows;
-    private SystemSettingSeekBarPreference mQqsRows;
-    
+    private Context mContext;
+
+    private CustomSeekBarPreference mQsColumns;
+    private CustomSeekBarPreference mQsRows;
+    private CustomSeekBarPreference mQqsRows;
+
+    private Button mApplyChange;
+
     private SystemSettingSwitchPreference mHide;
     private SystemSettingSwitchPreference mVertical;
+
+    private int[] currentValue = new int[2];
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         addPreferencesFromResource(R.xml.qs_tile_layout);
+    }
 
-        final int qs_rows = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QS_LAYOUT_ROWS, 4, UserHandle.USER_CURRENT);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        mQsRows = (SystemSettingSeekBarPreference) findPreference(KEY_QS_ROW_PORTRAIT);
+        mQsColumns = (CustomSeekBarPreference) findPreference(KEY_QS_COLUMN_PORTRAIT);
+        mQsColumns.setOnPreferenceChangeListener(this);
+
+        mQsRows = (CustomSeekBarPreference) findPreference(KEY_QS_ROW_PORTRAIT);
         mQsRows.setOnPreferenceChangeListener(this);
 
-        mQqsRows = (SystemSettingSeekBarPreference) findPreference(KEY_QQS_ROW_PORTRAIT);
-        updateQqsRowsLimit(qs_rows);
+        mQqsRows = (CustomSeekBarPreference) findPreference(KEY_QQS_ROW_PORTRAIT);
+        mQqsRows.setOnPreferenceChangeListener(this);
+
+        mContext = getContext();
+
+        LayoutPreference preference = findPreference(KEY_APPLY_CHANGE_BUTTON);
+        mApplyChange = (Button) preference.findViewById(R.id.apply_change);
+        mApplyChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mApplyChange.isEnabled()) {
+                    final int[] newValue = {
+                        mQsRows.getValue() * 10 + mQsColumns.getValue(),
+                        mQqsRows.getValue() * 10 + mQsColumns.getValue()
+                    };
+                    Settings.System.putIntForUser(getContentResolver(),
+                            Settings.System.QS_LAYOUT, newValue[0], UserHandle.USER_CURRENT);
+                    Settings.System.putIntForUser(getContentResolver(),
+                            Settings.System.QQS_LAYOUT, newValue[1], UserHandle.USER_CURRENT);
+                    if (QSLayoutUtils.updateLayout(mContext)) {
+                        currentValue[0] = newValue[0];
+                        currentValue[1] = newValue[1];
+                        mApplyChange.setEnabled(false);
+                    } else {
+                        Settings.System.putIntForUser(getContentResolver(),
+                                Settings.System.QS_LAYOUT, currentValue[0], UserHandle.USER_CURRENT);
+                        Settings.System.putIntForUser(getContentResolver(),
+                                Settings.System.QQS_LAYOUT, currentValue[1], UserHandle.USER_CURRENT);
+                        Toast.makeText(mContext, R.string.qs_apply_change_failed, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        initPreference();
 
         final boolean hideLabel = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QS_TILE_LABEL_HIDE , 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.QS_TILE_LABEL_HIDE, 0, UserHandle.USER_CURRENT) == 1;
 
         mHide = (SystemSettingSwitchPreference) findPreference(KEY_QS_HIDE_LABEL);
         mHide.setOnPreferenceChangeListener(this);
@@ -69,31 +122,50 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mQsRows) {
-            int qs_rows = Integer.parseInt(newValue.toString());
-            updateQqsRowsLimit(qs_rows);
-            return true;
-        } else if (preference == mHide) {
+        if (preference == mHide) {
             boolean hideLabel = (Boolean) newValue;
             mVertical.setEnabled(!hideLabel);
-            return true;
+        } else if (preference == mQsColumns) {
+            int qs_columns = Integer.parseInt(newValue.toString());
+            mApplyChange.setEnabled(
+                currentValue[0] != mQsRows.getValue() * 10 + qs_columns ||
+                currentValue[1] != mQqsRows.getValue() * 10 + qs_columns
+            );
+        } else if (preference == mQsRows) {
+            int qs_rows = Integer.parseInt(newValue.toString());
+            mQqsRows.setMax(qs_rows - 1);
+            if (mQqsRows.getValue() > qs_rows - 1) {
+                mQqsRows.setValue(qs_rows - 1);
+            }
+            mApplyChange.setEnabled(
+                currentValue[0] != qs_rows * 10 + mQsColumns.getValue() ||
+                currentValue[1] != mQqsRows.getValue() * 10 + mQsColumns.getValue()
+            );
+        } else if (preference == mQqsRows) {
+            int qqs_rows = Integer.parseInt(newValue.toString());
+            mApplyChange.setEnabled(
+                currentValue[0] != mQsRows.getValue() * 10 + mQsColumns.getValue() ||
+                currentValue[1] != qqs_rows * 10 + mQsColumns.getValue()
+            );
         }
-        return false;
-    }
-
-    private void updateQqsRowsLimit(int max_rows) {
-        mQqsRows.setMax(max_rows);
-        final int curr = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QQS_LAYOUT_ROWS, 2, UserHandle.USER_CURRENT);
-        if (curr > max_rows) {
-            mQqsRows.setValue(max_rows);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.QQS_LAYOUT_ROWS, max_rows, UserHandle.USER_CURRENT);
-        }
+        return true;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+
+    private void initPreference() {
+        final int index_qs = Settings.System.getIntForUser(getContentResolver(),
+            Settings.System.QS_LAYOUT, 42, UserHandle.USER_CURRENT);
+        final int index_qqs = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.QQS_LAYOUT, 22, UserHandle.USER_CURRENT);
+        mQsColumns.setValue(index_qs % 10);
+        mQsRows.setValue(index_qs / 10);
+        mQqsRows.setValue(index_qqs / 10);
+        mQqsRows.setMax(mQsRows.getValue() - 1);
+        currentValue[0] = index_qs;
+        currentValue[1] = index_qqs;
     }
 }

Subratam6296/Settings

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

src/com/android/settings/arrow/fragments/QsTileLayoutSettings.java
@@ -14,50 +14,104 @@
 
 package com.android.settings.arrow.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.catalyst.CatalystUtils.QSLayoutUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.arrow.support.preferences.SystemSettingSeekBarPreference;
+import com.android.settingslib.widget.LayoutPreference;
+
+import com.arrow.support.preferences.ProperSeekBarPreference;
+import com.arrow.support.preferences.SystemSettingSwitchPreference;
 
 public class QsTileLayoutSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_QS_ROW_PORTRAIT = "qs_layout_rows";
-    private static final String KEY_QQS_ROW_PORTRAIT = "qqs_layout_rows";
     private static final String KEY_QS_HIDE_LABEL = "qs_tile_label_hide";
     private static final String KEY_QS_VERTICAL_LAYOUT = "qs_tile_vertical_layout";
+    private static final String KEY_QS_COLUMN_PORTRAIT = "qs_layout_columns";
+    private static final String KEY_QS_ROW_PORTRAIT = "qs_layout_rows";
+    private static final String KEY_QQS_ROW_PORTRAIT = "qqs_layout_rows";
+    private static final String KEY_APPLY_CHANGE_BUTTON = "apply_change_button";
 
-    private SystemSettingSeekBarPreference mQsRows;
-    private SystemSettingSeekBarPreference mQqsRows;
+    private Context mContext;
+
+    private ProperSeekBarPreference mQsColumns;
+    private ProperSeekBarPreference mQsRows;
+    private ProperSeekBarPreference mQqsRows;
+
+    private Button mApplyChange;
 
     private SystemSettingSwitchPreference mHide;
     private SystemSettingSwitchPreference mVertical;
+
+    private int[] currentValue = new int[2];
 
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         addPreferencesFromResource(R.xml.qs_tile_layout);
+    }
 
-        final int qs_rows = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QS_LAYOUT_ROWS, 4, UserHandle.USER_CURRENT);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        mQsRows = (SystemSettingSeekBarPreference) findPreference(KEY_QS_ROW_PORTRAIT);
+        mQsColumns = (ProperSeekBarPreference) findPreference(KEY_QS_COLUMN_PORTRAIT);
+        mQsColumns.setOnPreferenceChangeListener(this);
+
+        mQsRows = (ProperSeekBarPreference) findPreference(KEY_QS_ROW_PORTRAIT);
         mQsRows.setOnPreferenceChangeListener(this);
 
-        mQqsRows = (SystemSettingSeekBarPreference) findPreference(KEY_QQS_ROW_PORTRAIT);
-        updateQqsRowsLimit(qs_rows);
+        mQqsRows = (ProperSeekBarPreference) findPreference(KEY_QQS_ROW_PORTRAIT);
+        mQqsRows.setOnPreferenceChangeListener(this);
+
+        mContext = getContext();
+
+        LayoutPreference preference = findPreference(KEY_APPLY_CHANGE_BUTTON);
+        mApplyChange = (Button) preference.findViewById(R.id.apply_change);
+        mApplyChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mApplyChange.isEnabled()) {
+                    final int[] newValue = {
+                        mQsRows.getValue() * 10 + mQsColumns.getValue(),
+                        mQqsRows.getValue() * 10 + mQsColumns.getValue()
+                    };
+                    Settings.System.putIntForUser(getContentResolver(),
+                            Settings.System.QS_LAYOUT, newValue[0], UserHandle.USER_CURRENT);
+                    Settings.System.putIntForUser(getContentResolver(),
+                            Settings.System.QQS_LAYOUT, newValue[1], UserHandle.USER_CURRENT);
+                    if (QSLayoutUtils.updateLayout(mContext)) {
+                        currentValue[0] = newValue[0];
+                        currentValue[1] = newValue[1];
+                        mApplyChange.setEnabled(false);
+                    } else {
+                        Settings.System.putIntForUser(getContentResolver(),
+                                Settings.System.QS_LAYOUT, currentValue[0], UserHandle.USER_CURRENT);
+                        Settings.System.putIntForUser(getContentResolver(),
+                                Settings.System.QQS_LAYOUT, currentValue[1], UserHandle.USER_CURRENT);
+                        Toast.makeText(mContext, R.string.qs_apply_change_failed, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        initPreference();
 
         final boolean hideLabel = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QS_TILE_LABEL_HIDE , 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.QS_TILE_LABEL_HIDE, 0, UserHandle.USER_CURRENT) == 1;
 
         mHide = (SystemSettingSwitchPreference) findPreference(KEY_QS_HIDE_LABEL);
         mHide.setOnPreferenceChangeListener(this);
@@ -68,31 +122,50 @@ public class QsTileLayoutSettings extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mQsRows) {
-            int qs_rows = Integer.parseInt(newValue.toString());
-            updateQqsRowsLimit(qs_rows);
-            return true;
-        } else if (preference == mHide) {
+        if (preference == mHide) {
             boolean hideLabel = (Boolean) newValue;
             mVertical.setEnabled(!hideLabel);
-            return true;
+        } else if (preference == mQsColumns) {
+            int qs_columns = Integer.parseInt(newValue.toString());
+            mApplyChange.setEnabled(
+                currentValue[0] != mQsRows.getValue() * 10 + qs_columns ||
+                currentValue[1] != mQqsRows.getValue() * 10 + qs_columns
+            );
+        } else if (preference == mQsRows) {
+            int qs_rows = Integer.parseInt(newValue.toString());
+            mQqsRows.setMax(qs_rows - 1);
+            if (mQqsRows.getValue() > qs_rows - 1) {
+                mQqsRows.setValue(qs_rows - 1);
+            }
+            mApplyChange.setEnabled(
+                currentValue[0] != qs_rows * 10 + mQsColumns.getValue() ||
+                currentValue[1] != mQqsRows.getValue() * 10 + mQsColumns.getValue()
+            );
+        } else if (preference == mQqsRows) {
+            int qqs_rows = Integer.parseInt(newValue.toString());
+            mApplyChange.setEnabled(
+                currentValue[0] != mQsRows.getValue() * 10 + mQsColumns.getValue() ||
+                currentValue[1] != qqs_rows * 10 + mQsColumns.getValue()
+            );
         }
-        return false;
-    }
-
-    private void updateQqsRowsLimit(int max_rows) {
-        mQqsRows.setMax(max_rows);
-        final int curr = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QQS_LAYOUT_ROWS, 2, UserHandle.USER_CURRENT);
-        if (curr > max_rows) {
-            mQqsRows.setValue(max_rows);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.QQS_LAYOUT_ROWS, max_rows, UserHandle.USER_CURRENT);
-        }
+        return true;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.ARROW;
+    }
+
+    private void initPreference() {
+        final int index_qs = Settings.System.getIntForUser(getContentResolver(),
+            Settings.System.QS_LAYOUT, 42, UserHandle.USER_CURRENT);
+        final int index_qqs = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.QQS_LAYOUT, 22, UserHandle.USER_CURRENT);
+        mQsColumns.setValue(index_qs % 10);
+        mQsRows.setValue(index_qs / 10);
+        mQqsRows.setValue(index_qqs / 10);
+        mQqsRows.setMax(mQsRows.getValue() - 1);
+        currentValue[0] = index_qs;
+        currentValue[1] = index_qqs;
     }
 }